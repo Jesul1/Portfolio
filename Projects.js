1b85bbd4bbe041ed309991f1db01add05a3a2713
Jesul1 <omajesse.vaarala@gmail.com>
2023-01-22 14:45:55 +0200

    Added Work experience section

---
+++ Projects.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#gameButton').hover(function() {
+      $('#gameIcon').css('background-color', 'yellow');
+    }, function() {
+      $('#gameIcon').css('background-color', '');
+    });
+  });
